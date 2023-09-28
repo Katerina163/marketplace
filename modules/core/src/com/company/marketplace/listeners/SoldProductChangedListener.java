f479da972a56aa695025b5634b50d7c00aca567a
Katerina Nefedova <Nefedova.Katia@yandex.ru>
2023-09-28 11:41:08 +0400

    EventListener теперь работает

--- modules/core/src/com/company/marketplace/listeners/SoldProductChangedListener.java
+++ modules/core/src/com/company/marketplace/listeners/SoldProductChangedListener.java
@@ -2,6 +2,7 @@ package com.company.marketplace.listeners;
 
 import com.company.marketplace.entity.PriceHistory;
 import com.company.marketplace.entity.SoldProduct;
+import com.haulmont.cuba.core.TransactionalDataManager;
 import com.haulmont.cuba.core.app.events.EntityChangedEvent;
 import com.haulmont.cuba.core.app.events.EntityPersistingEvent;
 import com.haulmont.cuba.core.entity.contracts.Id;
@@ -17,26 +18,29 @@ import java.util.UUID;
 public class SoldProductChangedListener {
     @Inject
     private DataManager dataManager;
+    @Inject
+    private TransactionalDataManager tdm;
 
     @EventListener
     public void beforePersist(EntityPersistingEvent<SoldProduct> event) {
-        createPriceHistory(event.getEntity());
+        SoldProduct sp = event.getEntity();
+        PriceHistory ph = dataManager.create(PriceHistory.class);
+        ph.setProduct(sp.getProduct());
+        ph.setShop(sp.getShop());
+        ph.setDatePriceChange(LocalDateTime.now());
+        dataManager.commit(ph);
     }
 
     @EventListener
     public void onSoldProductAfterCommit(EntityChangedEvent<SoldProduct, UUID> event) {
         Id<SoldProduct, UUID> entityId = event.getEntityId();
         if (event.getType().equals(EntityChangedEvent.Type.UPDATED) && event.getChanges().isChanged("price")) {
-            SoldProduct sp = dataManager.load(entityId).one();
-            createPriceHistory(sp);
+            SoldProduct sp = tdm.load(entityId).view("view-soldProduct-with-shop").one();
+            PriceHistory ph = tdm.create(PriceHistory.class);
+            ph.setProduct(sp.getProduct());
+            ph.setShop(sp.getShop());
+            ph.setDatePriceChange(LocalDateTime.now());
+            tdm.save(ph);
         }
-    }
-
-    private void createPriceHistory(SoldProduct sp) {
-        PriceHistory ph = dataManager.create(PriceHistory.class);
-        ph.setProduct(sp.getProduct());
-        ph.setShop(sp.getShop());
-        ph.setDatePriceChange(LocalDateTime.now());
-        dataManager.commit(ph);
     }
 }
