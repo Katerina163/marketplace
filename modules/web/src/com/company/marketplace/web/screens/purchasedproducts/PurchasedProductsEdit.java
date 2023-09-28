97f6cf18ccb5749bfa9ddf3c21902eae0d61a767
Katerina Nefedova <Nefedova.Katia@yandex.ru>
2023-09-28 08:09:11 +0400

    Корректировка покупаемых продуктов

--- modules/web/src/com/company/marketplace/web/screens/purchasedproducts/PurchasedProductsEdit.java
+++ modules/web/src/com/company/marketplace/web/screens/purchasedproducts/PurchasedProductsEdit.java
@@ -1,11 +1,29 @@
 package com.company.marketplace.web.screens.purchasedproducts;
 
-import com.haulmont.cuba.gui.screen.*;
 import com.company.marketplace.entity.PurchasedProducts;
+import com.company.marketplace.entity.Shop;
+import com.company.marketplace.entity.SoldProduct;
+import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.model.CollectionLoader;
+import com.haulmont.cuba.gui.screen.*;
+
+import javax.inject.Inject;
 
 @UiController("marketplace_PurchasedProducts.edit")
 @UiDescriptor("purchased-products-edit.xml")
 @EditedEntityContainer("purchasedProductsDc")
 @LoadDataBeforeShow
 public class PurchasedProductsEdit extends StandardEditor<PurchasedProducts> {
+    @Inject
+    private CollectionLoader<SoldProduct> productsDc;
+
+    @Subscribe("shopField")
+    public void onShopFieldValueChange(HasValue.ValueChangeEvent<Shop> event) {
+        if (event.getValue() != null) {
+            productsDc.setParameter("shop", event.getValue());
+        } else {
+            productsDc.removeParameter("shop");
+        }
+        productsDc.load();
+    }
 }
